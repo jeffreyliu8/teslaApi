cc245536652b0ae6ba7f71875c82a38f39f10ff2
Jeffrey Liu <jeffreyliu@gmail.com>
2018-06-08 15:28:41 -0700

    fix post

--- app/src/main/java/com/askjeffreyliu/teslaapi/endpoint/VehiclesEndpoint.java
+++ app/src/main/java/com/askjeffreyliu/teslaapi/endpoint/VehiclesEndpoint.java
@@ -22,6 +22,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
@@ -51,6 +52,7 @@ public class VehiclesEndpoint extends BaseEndpoint {
         Call<SimplePostResponse> honkHorn(@Header("Authorization") String authHeader, @Path("id") long id);
 
         @POST("api/1/vehicles/{id}/command/trunk_open")
+        @FormUrlEncoded
         Call<SimplePostResponse> openTrunk(@Header("Authorization") String authHeader, @Path("id") long id, @Field("which_trunk") String which_trunk);
     }
 
